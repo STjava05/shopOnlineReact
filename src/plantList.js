080da91f865a87b09417b23ab85ce3f2f0c7a031
Samuel Carrubba <97916309+carrubbasamuel@users.noreply.github.com>
2023-05-30 18:54:35 +0200

    first commit

--- src/plantList.js
+++ src/plantList.js
@@ -3,9 +3,7 @@ import { plantList } from "./dataPlant.js";
 
 function DisplayPlantList() {
     return(<ul>
-        {plantList.map((plant) => (<li key={plant.id} >{plant.name}{plant.isBestSale &&
-            <span>🔥</span>
-        }</li>))}
+        {plantList.map((plant) => (<li key={plant.id} >{plant.name}{plant.isBestSale && <span>🔥</span> }</li>))}
     </ul>)
 
 }
